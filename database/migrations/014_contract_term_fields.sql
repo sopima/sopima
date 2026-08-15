cf16e1b9f9120299eb396d8d98e0046e8359e67a
Berrent <darius.suchanek@gmail.com>
2026-08-15 16:09:53 +0200

    fix: remove duplicate column migrations 014 and 015

--- database/migrations/014_contract_term_fields.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE contracts
-    ADD COLUMN minimum_term_months INT NULL DEFAULT NULL,
-    ADD COLUMN renewal_interval_months INT NULL DEFAULT NULL;
--- database/migrations/015_contract_direction.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE contracts
-    ADD COLUMN direction VARCHAR(10) NOT NULL DEFAULT 'ausgabe';
-
-UPDATE contracts SET direction = 'einnahme' WHERE source = 'api';
